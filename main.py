3f0b6767aa1cae8b792ec598fb207480aa42c301
Sujay G Kaushik <110559675+sgk0704@users.noreply.github.com>
2023-10-08 18:54:12 +0530

    main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, Response
-import os
+from flask_socketio import SocketIO, emit
 import cv2
+import os
 import cvzone
 from cvzone.PoseModule import PoseDetector
 
 app = Flask(__name__)
-
+socketio = SocketIO(app, async_mode='eventlet')
 
 def gen_frames():
     camera = cv2.VideoCapture(0)
@@ -37,16 +38,15 @@ def gen_frames():
             imgShirt = cv2.imread(os.path.join(shirtFolderPath, listShirts[imageNumber]), cv2.IMREAD_UNCHANGED)
             
             widthOfShirt = int((lm11[0] - lm12[0]) * fixedRatio)
-            print(widthOfShirt)
             imgShirt = cv2.resize(imgShirt, (widthOfShirt, int(widthOfShirt * shirtRatioHeightWidth)))
             currentScale = (lm11[0] - lm12[0]) / 190
             offset = int(44 * currentScale), int(48 * currentScale)
 
             try:
                 img = cvzone.overlayPNG(img, imgShirt, (lm12[0] - offset[0], lm12[1] - offset[1]))
-            except:
+            except Exception as e:
                 pass
-                #print(f"Error overlaying shirt: {e}")
+                # Handle overlaying error
 
             img = cvzone.overlayPNG(img, imgButtonRight, (1074, 293))
             img = cvzone.overlayPNG(img, imgButtonLeft, (72, 293))
@@ -74,18 +74,32 @@ def gen_frames():
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
-    camera.release()
 
+    camera.release()
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@socketio.on('connect', namespace='/video_socket')
+def test_connect():
+    print('Client connected')
+
+@socketio.on('disconnect', namespace='/video_socket')
+def test_disconnect():
+    print('Client disconnected')
+
+@socketio.on('frame', namespace='/video_socket')
+def handle_frame(frame):
+    # Process the frame as needed (if required)
+    # You can add more processing here...
+
+    # Send the processed frame back to the client (if needed)
+    emit('processed_frame', frame, namespace='/video_socket', broadcast=True)
 
 @app.route('/video_feed')
 def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app, debug=True)
